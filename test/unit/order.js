c0110d2cdf1808a02eb5ee35848a3706cb697800
westumdenchev <mdenchev@westum.com>
2019-08-22 12:21:18 +0300

    Add more tests

--- test/unit/order.js
+++ test/unit/order.js
@@ -6,11 +6,11 @@ let proxyquire = require('proxyquire').noCallThru();
 
 let MailService = require('../../lib/mail');
 
-let sendSpy = sinon.fake();
+let fakeSendFuncCall = sinon.fake();
 
 class MailServiceMock extends MailService {
 	send() {
-		sendSpy();
+		fakeSendFuncCall();
 	}
 }
 
@@ -18,18 +18,28 @@ let Order = proxyquire('../../lib/order', {
 	'./mail': MailServiceMock
 });
 
-describe('order', function () {
-	context('place', function () {
+describe('order', () => {
+	context('place', () => {
 		
-		it('should notify customer', function () {
+		it('should notify customer', () => {
 			let order = new Order();
 			order.setAmount(9.99);
 			order.place();
 
-			expect(sendSpy.called).to.be.true;
+			expect(fakeSendFuncCall.called).to.be.true;
 		});
 
-		it('should get the same value as it was set', function () {
+		it('should call mailService->send after successful payment', () => {
+			let order = new Order();
+			let spy = sinon.spy(order,'settlePayment');
+
+			order.setAmount(9.99);
+			order.place();
+
+			expect(fakeSendFuncCall.calledAfter(spy)).to.be.true;
+		});
+
+		it('should get the same value as it was set', () => {
 			let order = new Order();
 			let spy = sinon.spy(order,'getAmount');
 
@@ -39,7 +49,7 @@ describe('order', function () {
 			expect(spy.returnValues[0]).to.be.equal(9.99);
 		});
 
-		it('should call and settle payment', function () {
+		it('should call and settle payment', () => {
 			let order = new Order();
 			order.setAmount(10.99);
 
@@ -52,7 +62,7 @@ describe('order', function () {
 			expect(result).to.be.true;
 		});
 
-		it('should throw exception if payment does not go through', function () { 
+		it('should throw exception if payment does not go through', () => { 
 			var order = new Order();
 
 			order.setAmount(9.99);
